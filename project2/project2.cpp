9f300f166fed87f60264de825dab3a26d3bd001a
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-10-10 14:27:02 +0300

    Добавьте файлы проекта.

---
+++ project2/project2.cpp
@@ -0,0 +1,20 @@
+﻿// project2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int oc1 = 5, oc2 = 4, oc3 = 3, sr = (oc1 + oc2 + oc3) / 3;
+    cout << "Имя студента: Иван Иванов" << endl;
+    cout << "Фамилия студента: Петров" << endl;
+    
+    cout << "Оценки за экзамен: " << oc1 << " " << oc2 << " " << oc3 << " " <<  endl;
+    
+    cout << "Средний балл студента: " << sr << endl;
+
+    return 0;
+}
+
